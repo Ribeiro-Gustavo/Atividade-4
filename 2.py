dff7959b7e64dcb8b636251b9a2503efd09e43b0
Ribeiro-Gustavo <141081207+Ribeiro-Gustavo@users.noreply.github.com>
2023-10-09 23:36:59 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,30 @@
+import random
+
+# Listas de nomes e sobrenomes
+nomes = ["Cecilia", "Joana", "Mariana", "Pericles", "Luísa", "Rafael", "Laura", "Carlos", "Evaristo", "Lucas",
+         "Lívia", "Barbara", "Juliana", "Ronaldo", "Marciano", "Fiona", "Caciano", "Daniela", "Patrícia"]
+sobrenomes = ["Kaminski", "Kramer", "Oliveira", "Silva", "Ferreira", "Rodrigues", "Sanches", "Lima", "Souza", "Costa",
+              "Carvalho", "Chemeres", "Ribeiro", "Araújo", "Martins", "Cavalcanti", "Freitas", "Correia", "Vieira"]
+
+def gera_dados_aleatorios(N):
+    with open("dados_pessoais.txt", "w") as arquivo:
+        for _ in range(N):
+            nome_completo = f"{random.choice(nomes)} {random.choice(sobrenomes)}"
+            idade = random.randint(18, 60)
+            altura = round(random.uniform(1.50, 2.00), 2)
+            arquivo.write(f"{nome_completo}, {idade} anos, {altura} m\n")
+
+N = int(input("Digite o número de registros a serem gerados: "))
+gera_dados_aleatorios(N)
+
+
+def escreverNumerosAleatorios(qtdNumeros, nomeArquivo):
+    arquivoNumeros = open(nomeArquivo, 'w')
+    for i in range(qtdNumeros):
+        nome_completo = f"{random.choice(nomes)} {random.choice(sobrenomes)}"
+        idade = random.randint(18, 60)
+        altura = round(random.uniform(1.50, 2.00), 2)
+        arquivoNumeros.write(f"{nome_completo}, {idade} anos, {altura} m\n")
+    arquivoNumeros.close()
+
+escreverNumerosAleatorios(100, 'cadastro.txt')
